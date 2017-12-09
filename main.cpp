43185d5e5b0115fd23ea7da7db0613fd4b3ac317
anicanor <anicanor@mail.csuchico.edu>
2017-12-09 01:17:40 -0800

    done

--- main.cpp
+++ main.cpp
@@ -1,183 +1,144 @@
 //Author: Aaron Nicanor
 #include <string>
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include "room.h"
-#include "item.h"
+#include <fstream>
+#include "enemy.h"
 
 using namespace std;
 
-//Prototypes
-void levelSetUp(room&);
-void recordKeeper(string);
-void puzzleSolvedRoom(room&);
+double playerWinRatio(int, int);
+void playerRecordWin(int&, int&);
+bool winningPerson(string, string);
+void finalTally(double, double, int, vector<string>, vector<string>);
+vector <string> playerRecords;
+
 int main(){
     
+    string playerDecision;
     char input;
-    bool happyEnd, sadEnd;
-    room currentRoom;
-    levelSetUp(currentRoom);
-    vector <item> inventory;
-    cout<<"You awaken in some unknown room. You carry nothing in your possession and have no idea where you are, but task yourself to find an exit from this strange place."<<endl;
+    int games, playerWins;
+    enemy ai;
+    ai.initialWins();
+    bool endGames;
+    games = 0;
+    playerWins = 0;
+
     
     do{
-        
-        if(currentRoom.finalRoom() == true){
+    
+        cout<<"(P)lay, see (W)in ratio, (Q)uit"<<endl;
+        cin>>input;
+
+        if( input == 'p' || input == 'P' ){
             
-            cout<<"You dash into the wide open area."<<endl<<"You are free!"<<endl;
-            happyEnd = true;
+            cout<<"Input your choice:"<<endl<<"rock, paper, or scissors?"<<endl<<"(please input it in all lower case)"<<endl;
+            cin>>playerDecision; 
+            playerRecords.push_back(playerDecision);
+            
+            if( winningPerson(playerDecision, ai.choice()) == true ){
+            
+                cout<<"You win!"<<endl;
+                playerRecordWin(playerWins, games);
+
+            }else{
+                
+                cout<<"You lose!"<<endl;
+                ai.recordWin(games);
+
+            }
+
+        }else if( input == 'w' || input == 'W' ){
+            
+            cout<<"Who's win ratio do you want to see?"<<endl<<"mine or enemy"<<endl<<"(please input it in all lower case)"<<endl;
+            cin>>playerDecision;
+
+            if( playerDecision == "mine" ){
+                
+                cout<<"Your win ratio is "<<playerWinRatio(playerWins, games)<<endl;
+
+            }else if( playerDecision == "enemy" ){
+                
+                cout<<"The enemy's win ratio is "<<ai.winRatio(games)<<endl;
+
+            }else{
+
+                cout<<"I'm sorry, I don't recognize that responce. Try either mine or enemy next time."<<endl;
+
+            }
+        
+        }else if( input == 'q' || input == 'Q' ){
+            
+            endGames = true;
+            finalTally(playerWinRatio(playerWins, games), ai.winRatio(games), games, playerRecords, ai.getVector());
 
         }else{
-        currentRoom.readDesc();
-        cout<<"Currently, you can (M)ove to another room, (T)ake an item in a room, (C)heck you inventory, (U)se an item in your inventory, or (Q)uit the game."<<endl;
-        cin>>input;
-        
-        if( input=='m' || input=='M' ){
-            
-            cout<<"What direction do you wish to go?"<<endl<<"(N)orth, (E)ast, (S)outh, or (W)est?"<<endl;
-            cin>>input;
-            
-            if( currentRoom.hasExit(input)==true ){
-                
-                currentRoom = currentRoom.changeRoom(input);
 
-            }else{
-
-            }
-
-        }else if( input=='t' || input=='T' ){
-
-            if(currentRoom.checkItem() == true){
-                
-                item take;
-                currentRoom.grabItem(take);
-                inventory.push_back(take);
-                cout<<"You successfully taken the "<<take.name()<<endl;
-
-            }else{
-
-                cout<<"There's nothing to grab from this room."<<endl;
-
-            }
-
-        }else if( input=='c' || input=='C' ){
-            
-            if (inventory.size() == 0){
-                
-                cout<<"Your inventory is empty"<<endl;
-
-            }else{
-                
-                cout<<"Within your inventory, you have ";
-                for(int i = 0; i < inventory.size(); i++){
-                    
-                    cout<<"the "<<inventory[i].name()<<" ";
-
-                }
-                cout<<endl;
-
-           }
-
-       }else if( input=='u' || input=='U' ){
-
-               cout<<"What item would you like to use? (Type the item's name in all lower case letters)"<<endl;
-               cin>>input;
-               item chosen;
-               chosen.includeItem(false);
-
-               for(int i = 0; i < inventory.size(); i++){
-                
-                    if (input == inventory[i].name()){
-                        
-                        chosen = inventory[i];
-                        chosen.includeItem(true);
-
-                    }
-               }
-
-               if (chosen.checkIfReal() == false){
-                
-                    cout<<"Seems like you don't have that item in your inventory."<<endl;
-
-               }else{
-
-                    if(currentRoom.puzzleSolver(chosen) == true){
-                        
-                        puzzleSolvedRoom(currentRoom);
-
-                    }
-               }
-    
-
-        }else if( input=='q' || input=='Q' ){
-
-                sadEnd = true;
-                cout<<"You slip on some foreign object on the floor and fall hard onto the floor. Sadly, you don't survive you rather pitiful tumble. This is where you meet your end..."<<endl;
+            cout<<"I'm sorry, I don't quite recognize that response. Try either p, w, or q next time."<<endl;
 
         }
+
+    }while (endGames != true);
+
+    return 0;
+
+}
+
+double playerWinRatio(int pWins, int total){
+    
+    double ratio;
+    ratio = pWins/total;
+    return ratio;
+
+}
+
+void playerRecordWin(int &playerWins, int &games){
+    
+    playerWins++;
+    games++;
+
+}
+
+bool winningPerson(string player, string enemy){
+    
+    if( player == enemy ){
+
+        return true;
+
+    }else if( player == "scissor" && enemy == "paper" ){
+        
+        return true;
+
+    }else if( player == "rock" && enemy == "scissor" ){
+        
+        return true;
+
+    }else if( player == "paper" && enemy == "rock" ){
+        
+        return true;
+
+    }else{
+        
+        return false;
+
     }
+}
 
-    }while(happyEnd == true || sadEnd == true);
+void finalTally(double playerRatio, double enemyRatio, int games, vector<string> player, vector<string> enemy){
     
-    cout<<"Thanks for playing!"<<endl;
-}
-
-void levelSetUp(room &startingRoom){
-
-    room one, two, three, four, five, six, seven;
-    item smallKey, bucketOfWater;
-    //gives the items their names and IDs
-    smallKey.giveName("Small Key");
-    smallKey.giveID(1);
-    bucketOfWater.giveName("Bucket of Water");
-    bucketOfWater.giveID(4);
-    //sets up exits
-    one.setExits(true, false, false, false);
-    two.setExits(false, false, true, false);
-    three.setExits(false, true, true, true);
-    four.setExits(false, true, false, false);
-    five.setExits(false, false, false, true);
-    six.setExits(true, false, true, false);
-    seven.setExits(false, false, false, false);
-    //sets up descriptions of rooms
-    one.setDesc("It's an empty room with a single door due north. Upon closer inspection of the seemingly barren room, you can see a small key.");
-    two.setDesc("You find yourself in a hallway with a locked door due north and the empty room due south. Perhaps that locked doo could be opened with some kind of key...");
-    three.setDesc("This room looks like some kind of living room. To your west, there's a small closet and to your east, there's an archway leading to another room. There's also a room due south.");
-    four.setDesc("You cram yourself into the rather tight closet. Within it, you see a bucket filled with water. The only exit is to your east.");
-    five.setDesc("You step inside a very comfy looking room. To your north you see a fireplace that has been lit. If you look closey, you can see that there's someting behind the fire. There's also an exit to your west.");
-    six.setDesc("The room you enter is filled with fresh air. You can see the outside just north from where you are, finally, an escape! Or, if you really find it nice in here, you can always head back south.");
-    //gives certain rooms items
-    one.giveItem(smallKey);
-    four.giveItem(bucketOfWater);
-    //connects the rooms
-    one.setRooms(two, 'n');
-    two.setRooms(one, 's');
-    three.setRooms(two, 's');
-    three.setRooms(four, 'w');
-    three.setRooms(five, 'e');
-    four.setRooms(three, 'e');
-    five.setRooms(three, 'w');
-    six.setRooms(five, 's');
-    six.setRooms(seven, 'n');
-    //gives rooms IDs
-    one.setID(1);
-    two.setID(2);
-    three.setID(3);
-    four.setID(4);
-    five.setID(5);
-    six.setID(6);
-    //sets starting point
-    startingRoom = one;
-    //sets ending point
-    seven.setEndgame();
+    ofstream outFile;
+    outFile.open("record_books.txt");
+    if (outFile.is_open()){
+        outFile<<"This rock paper sciccors match ended with the player having a winning ratio of "<< playerRatio <<"."<<endl<<"and the enemy with a ratio of "<<enemyRatio<<"."<<endl<<"In total, the amount of games played were "<<games<<" games in total!"<<endl;
+        for(int i = 0; i < games; i++){
+            outFile<<"On match "<<i+1<<" you used "<<player[i]<<" and the enemy used "<<enemy[i]<<"."<<endl;
+        }
+        outFile<<"I hope you come around to play again!"<<endl<<endl;
+    }
+    outFile.close();
 
 }
 
-void puzzleSolvedRoom(room &startingRoom){
-    
-}
 
-void recordKeeper(string input){
 
-}
+
